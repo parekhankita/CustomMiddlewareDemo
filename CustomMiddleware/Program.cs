b78ea8875768bf292c57918ffcf2a3409ece4456
Ankita <ankita.parekh@praxicaltech.com>
2025-11-09 16:17:44 +0530

    Middleware implementation

--- CustomMiddleware/Program.cs
+++ CustomMiddleware/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Use your custom middleware
+app.UseMiddleware<SimpleMiddleware>();
+
+app.MapGet("/", () => "Hello from Middleware!");
+
+app.Run();
---
+++ CustomMiddleware/SimpleMiddleware.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CustomMiddleware
+{
+    public class SimpleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SimpleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("➡️ Before Request");
+
+            await _next(context); // call the next middleware
+
+            Console.WriteLine("⬅️ After Request");
+        }
+    }
+}
